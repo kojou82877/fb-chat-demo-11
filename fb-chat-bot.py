3a6685192bd61051c87ddceed66b1f58caee17b1
kojou82877 <116699713+kojou82877@users.noreply.github.com>
2022-10-26 14:19:56 +0530

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -491,7 +491,7 @@ class ChatBot(Client):
                 reply = "Yes, if you are nice and kind girl. But if you are boy RIP."
                 sendMsg()
             elif ("you from" in msg):
-                reply = "I am from Nepal. Currently living in Kathmandu"
+                reply = "I am from India. Currently living in Kathmandu"
                 sendMsg()
             elif ("you sure" in msg):
                 reply = "Yes. I'm sure."
